34d7bd3258604c37baf75852d732d6e9dd2d8b90
kamyar kamali kamazani <kamyarkamali021@gmail.com>
2023-04-13 01:50:33 +0430

    Continuing the project and building the author component

--- src/Components/author/AuthorPage.jsx
+++ src/Components/author/AuthorPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useParams} from "react-router-dom";
 import {useQuery} from"@apollo/client";
 import { GET_AUTHOR } from '../../graphql/queryse';
+import {Avatar, Container, Grid, Typography} from "@mui/material"
 
 function AuthorPage() {
     const {slug}=useParams()
@@ -10,7 +11,25 @@ function AuthorPage() {
     })
     console.log(data)
   return (
-    <div>AuthorPage</div>
+   <>
+        <Container maxWidth="lg">
+
+            <Grid container mt={10}>
+
+            <Grid xs={12} item display="flex" flexDirection="column" alignItems="center">
+            <Avatar src={data.author.avatar.url} sx={{width:250,height:250}}/>
+            <Typography mt={4} color="#777" fontWeight={700} component="h3" variant='h5'>{data.author.name}</Typography>
+            <Typography mt={2} color="gray" component="p" variant='h5'>{data.author.field}</Typography>
+            </Grid>
+
+            <Grid xs={12} item>
+                {data.author.description.html}
+            </Grid>
+
+            </Grid>
+
+        </Container>
+   </>
   )
 }
 
